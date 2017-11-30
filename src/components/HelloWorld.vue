ea669a9eac045d61ffde1d308df20740062dad46
wanggang4 <wanggang4@100tal.com>
2017-11-30 14:44:03 +0800

    dev

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,171 @@
+<template>
+    <div class="index-box">
+       <top></top>
+       <div class="main">
+           <el-tabs v-model="activeName" @tab-click="handleClick">
+               <el-tab-pane label="tab1" name="first">
+                   <el-input v-model="searchContent"
+                    prefix-icon="el-icon-search"></el-input>
+                   <el-button type="primary" @click="search">Query</el-button>
+                   <div class="card-content clear">
+                       <div class="list1 list">
+                           <span>list-1</span>
+                           <ul>
+                               <li v-for="(item,index) in list1" :key="item" @click.prevent="choseList1(index)"
+                                 :class="index == currentIndex1 ? 'li-active' : ''">{{"列表"+item}}</li>
+                           </ul>
+                       </div>
+                       <div class="list2 list">
+                           <span>list-2</span>
+                           <ul>
+                               <li v-for="(item,ltIndex) in list2" :key="item+ltIndex" @click.prevent="choseList2(ltIndex)" 
+                               :class="ltIndex == currentIndex2 ? 'li-active' : ''">{{"列表"+item}}</li>
+                           </ul>
+                       </div>
+                       <div class="table-list">
+                           <el-table :data="tableData" style="width: 100%" border stripe size="mini">
+                               <el-table-column v-for="(v,k,tIndex) in tableData[0]" :key="v" :label="k" :prop="k"></el-table-column>
+                               <el-table-column label="操作" fixed="right" width="100">
+                                    <template slot-scope="scope">
+                                        <el-button @click.native="edit(scope.row)" type="text" size="small">编辑</el-button>
+                                        <el-button @click.native="del(scope.$index, scope)" type="text" size="small">删除</el-button>
+                                   </template>
+                               </el-table-column>
+                           </el-table>
+                       </div>
+                   </div>
+               </el-tab-pane>
+               <el-tab-pane label="tab2" name="second">tab2</el-tab-pane>
+               <el-tab-pane label="tab3" name="third">tab3</el-tab-pane>
+           </el-tabs>
+       </div>
+    </div>
+</template>
+
+<script>
+    import Top from './top'
+    export default {
+        components: {
+            Top,
+
+        },
+        data(){
+            return {
+                activeName: 'first',
+                searchContent: '',
+                currentIndex1: 0,
+                currentIndex2: 0,
+                list1:[1,2,3,4],
+                list2: [5,6,7,8,9,10],
+                // tableDataCol: {},
+                // tableData: [],
+                tableDataCol: {id: 'id', name: 'name', page: 'page', delay: 'delay'},
+
+                tableData: [{
+                    id: 1,
+                    name: 'qqq',
+                    page: 'asdfsdf',
+                    delay: 0,
+                }, {
+                    id: 2,
+                    name: 'www',
+                    page: 'sdfsfsdafsadfsdfasd',
+                    delay: 0
+                }, {
+                    id: 3,
+                    name: 'egfdsgfdsee',
+                    page: 'gdfgdf',
+                    delay: 0
+                }],
+            }
+        },
+        methods: {
+            handleClick(tab, event){
+                console.log(tab, event)
+            },
+            search(){
+                alert(this.searchContent)
+            },
+            choseList1(index){
+                this.currentIndex1 = index
+            },
+            choseList2(index){
+                this.currentIndex2 = index
+            },
+            edit(row){
+                console.log(row)
+            },
+            del(index, scope){
+                this.tableData.splice(index, 1)
+            }
+        },
+    }
+</script>
+
+<style lang="scss">
+    .index-box{
+        background: #f0f0f0;
+        padding-bottom: 20px;
+        .main{
+            min-height: 800px;
+            margin: 20px 50px;
+            padding: 5px 20px;
+            border: 1px solid #DFE4ED;
+            border-radius: 5px;
+            background-color: #fff;
+            .el-tab-pane{
+                .el-input{
+                    width: 90%;
+                }
+            }
+            .card-content{
+                overflow: hidden;
+                .list{
+                    width: 10%;
+                    float: left;
+                    margin:15px 20px;
+                    font-size: 14px;
+                    &>span{
+                        display: block;
+                        background-color: #d9edf6;
+                        height: 36px;
+                        line-height: 36px;
+                        color: #466f85;
+                        text-align: center;
+                        margin-bottom: 8px;
+                        border-radius: 4px;
+                    }
+                    &>ul{
+                        border: 1px solid #e5e5e5;
+                        border-radius: 4px;
+                        &>li{
+                            border-top: 1px solid #e5e5e5;
+                            line-height: 25px;
+                            padding: 5px 10px;
+                            word-break: break-word;
+                            cursor: pointer;
+                        }
+                        &>li:first-child{
+                            border-top: none;
+                        }
+                        .li-active{
+                            background-color: #def0d8;
+                            color: #4d7f4e;
+                        }
+                    }
+                }
+                .table-list{
+                    float: left;
+                    width: 70%;
+                    padding-top: 15px;
+                }
+                .text{
+                    font-size: 14px;
+                }
+                .item{
+                    margin-bottom: 18px;
+                }
+            }
+        }
+    }
+</style>
---
+++ src/components/top.vue
@@ -0,0 +1,52 @@
+<template>
+	<div class="top-con clear">
+		<a href="#" class="left home">Home</a>
+		<div class="right right-con">
+			<a href="#">GitHub</a>
+			<el-dropdown trigger="click">
+			  <span class="el-dropdown-link">
+			    other<i class="el-icon-arrow-down el-icon--right"></i>
+			  </span>
+			  <el-dropdown-menu slot="dropdown">
+			    <el-dropdown-item><a href="http://www.baidu.com">aaa</a></el-dropdown-item>
+			    <el-dropdown-item><a href="#">bbb</a></el-dropdown-item>
+			  </el-dropdown-menu>
+			</el-dropdown>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		data(){
+			return {
+				
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+.top-con{
+	width: 100%;
+	height: 50px;
+	line-height: 50px;
+	background-color: #409EFF; 
+	& a{
+		color: #fff;
+	}
+	.home{
+		margin-left: 50px;
+	}
+	.right-con{
+		margin-right: 50px;
+		&>a{
+			margin-right: 20px;
+		}
+	}		    
+	.el-dropdown-link{
+		color: #fff;
+	}
+}
+	
+</style>
