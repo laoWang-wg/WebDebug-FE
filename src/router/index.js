039755872be09b8c9e71b5a3d0b8e77a1aa90dc9
wanggang4 <wanggang4@100tal.com>
2017-12-07 16:30:23 +0800

    add edit

--- src/router/index.js
+++ src/router/index.js
@@ -4,12 +4,13 @@ import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
+ const routes = [{
+ 	path: '/',
+ 	name: 'HelloWorld',
+ 	component: HelloWorld
+ }]
+
+
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+  routes
 })
